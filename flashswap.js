cdbedf131f35b2fb184a90badafa9488e875d5ea
supercoolx <dm.kondratov.git@gmail.com>
2022-06-11 10:57:40 -0700

    change flashswap

--- flashswap.js
+++ flashswap.js
@@ -9,16 +9,6 @@ const wallet = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY).address;
 
 const main = async () => {
 
-    // uniswapV3 addresses
-    const routerV3_addr = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
-    const factoryV3_addr = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
-    // uniswapV2 addresses
-    const routerV2_addr = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-    const factoryV2_addr = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-    // sushiswap address
-    const sushi_router_addr = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
-    const sushi_factory_addr = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
-    // token addresses
     const WETH_addr = '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
     const DAI_addr = '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa';
 
