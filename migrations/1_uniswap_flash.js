677585fca925a551fc6758cc2f045ea96b36d307
supercoolx <dm.kondratov.git@gmail.com>
2022-06-24 01:28:53 -0700

    some change

--- migrations/1_uniswap_flash.js
+++ migrations/1_uniswap_flash.js
@@ -4,17 +4,17 @@ const tokens = require("../config/token.json");
 const network = 'Mainnet';
 module.exports = async (deployer) => {
     // uniswapV3 addresses
-    const uniV3_router = dexes[network].UniswapV3.Router; // '0xE592427A0AEce92De3Edee1F18E0157C05861564';
-    const uniV3_factory = dexes[network].UniswapV3.Factory; // '0x1F98431c8aD98523631AE4a59f267346ea31F984';
+    const uniV3_router = dexes[network].UniSwapV3.Router; // '0xE592427A0AEce92De3Edee1F18E0157C05861564';
+    const uniV3_factory = dexes[network].UniSwapV3.Factory; // '0x1F98431c8aD98523631AE4a59f267346ea31F984';
     // uniswapV2 addresses
-    const uniV2_router = dexes[network].UniswapV2.Router; // '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-    const uniV2_factory = dexes[network].UniswapV2.Factory; // '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+    const uniV2_router = dexes[network].UniSwapV2.Router; // '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+    const uniV2_factory = dexes[network].UniSwapV2.Factory; // '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
     // sushiswap address
-    const sushi_router = dexes[network].Sushiswap.Router; // '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
-    const sushi_factory = dexes[network].Sushiswap.Factory; // '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
+    const sushi_router = dexes[network].SushiSwap.Router; // '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
+    const sushi_factory = dexes[network].SushiSwap.Factory; // '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
     // shibaswap address
-    const shiba_router = dexes[network].Shibaswap.Router; // '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
-    const shiba_factory = dexes[network].Shibaswap.Factory; // '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
+    const shiba_router = dexes[network].ShibaSwap.Router; // '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
+    const shiba_factory = dexes[network].ShibaSwap.Factory; // '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
     
     // token addresses
     const WETH = tokens[network].WETH; // '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
