ac623547ac3c24086aca98a16d005c198ab65c0d
supercoolx <dm.kondratov.git@gmail.com>
2022-06-30 10:42:32 -0700

    fix error

--- migrations/_mock_swap.js
+++
@@ -1,7 +0,0 @@
-const MockSwap = artifacts.require("MockSwap");
-module.exports = async function(deployer) {
-
-    await deployer.deploy(MockSwap);
-    await MockSwap.deployed();
-  // Use deployer to state migration tasks.
-};
